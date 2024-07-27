6d8b26438693e2a239d412b876e7d6549dde4ea9
hubbbbbbbbb <hubyu9234@gmail.com>
2024-07-27 10:44:28 -0700

    set up routes

--- math-souls/src/App.jsx
+++ math-souls/src/App.jsx
@@ -3,8 +3,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <BrowserRouter>
       <Routes>
