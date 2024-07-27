141232c878b08c673acdce0b184f87d0524fb245
Onlywar10 <adamchenbowen@gmail.com>
2024-07-27 12:11:52 -0700

    Question Page Started

--- math-souls/src/App.jsx
+++ math-souls/src/App.jsx
@@ -1,19 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-<<<<<<< HEAD
-=======
 import Login from "./pages/Login";
->>>>>>> f06c9f1631d93611d776ecf3400197dc07315b4e
-
+import QuestionPage from "./pages/QuestionPage";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-<<<<<<< HEAD
-=======
         <Route path="/login" element={<Login />} />
->>>>>>> f06c9f1631d93611d776ecf3400197dc07315b4e
+        <Route path="/question" element={<QuestionPage />} />
       </Routes>
     </BrowserRouter>
   );
