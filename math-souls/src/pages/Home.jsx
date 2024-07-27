15a68dbea241c8b08226b7d3b32e4e0270b2222d
Julian Lee <jlee793@ucmerced.edu>
2024-07-27 13:50:41 -0700

    Menu Added

--- math-souls/src/pages/Home.jsx
+++ math-souls/src/pages/Home.jsx
@@ -1,7 +1,45 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  
+  function handleClick() {
+    alert('You clicked me!');
+  }
+
+  
+
+  return (
+    <button onClick={handleClick}>button</button>
+  );
 };
+
+// function TextBoxButtonExample() {
+//   // State to store the input value
+//   const [inputValue, setInputValue] = useState('');
+
+//   // Handler for input change
+//   const handleInputChange = (event) => {
+//     setInputValue(event.target.value);
+//   };
+
+//   // Handler for button click
+//   const handleButtonClick = () => {
+//     alert(`Button clicked! Input value: ${inputValue}`);
+//   };
+
+//   return (
+//     <div>
+//       <input 
+//         type="text" 
+//         value={inputValue} 
+//         onChange={handleInputChange} 
+//         placeholder="Enter text here"
+//       />
+//       <button onClick={handleButtonClick}>
+//         Click Me
+//       </button>
+//     </div>
+//   );
+// }
 
 export default Home;
