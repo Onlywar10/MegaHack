616cd900c987c0ff8aea7cb33fd451a9d579c5ae
hubbbbbbbbb <hubyu9234@gmail.com>
2024-07-27 21:35:58 -0700

    completed gauntlet and updated API

--- math-souls/src/pages/Gauntlet.jsx
+++ math-souls/src/pages/Gauntlet.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import Box from "@mui/material/Box";
 import { LinearProgress } from "@mui/material";
 import axios from "axios";
 import { FaHeart } from "react-icons/fa";
+import useSound from "use-sound";
 
 import { supabase } from "../supabase";
+import { deathSound, enemyFelled } from "../assets";
 import "./Gauntlet.css";
 
 const Gauntlet = ({ setUserUpdate }) => {
@@ -37,63 +39,182 @@ const Gauntlet = ({ setUserUpdate }) => {
   const [playerLost, setPlayerLost] = useState(false);
 
   // quips for fun
-  const [quip, setQuip] = useState(quipList[0]);
+  const [quip, setQuip] = useState("");
 
+  // Hooks
   let { category } = useParams();
+  let navigate = useNavigate();
+  const [playDeathSound] = useSound(deathSound, { volume: 0.2 });
+  const [playVictorySound] = useSound(enemyFelled, { volume: 0.3 });
 
   const fetchGauntletQuestion = () => {
     axios
       .get("http://127.0.0.1:5000/question/?category=" + category)
       .then((res) => {
-        console.log(res.data);
         setGauntletQuestion(res.data);
       });
   };
 
   const checkAnswer = () => {
+    // console.log(userAnswer.replace(/\s+/g, ""));
+    // console.log(gauntletQuestion.solution.replace(/\s+/g, ""));
     if (userAnswer.trim() == "") {
       alert("Answer box is empty!");
     } else {
-      if (userAnswer.trim() == gauntletQuestion.solution.trim()) {
-        if (bossHealth - 10 == 0) {
-          setGameOver(true);
-          setDefeatedMonster(true);
-        } else {
-          setBossHealth(bossHealth - 10);
-          setQuestionComplete(true);
-          setQuestionCorrect(true);
-        }
-      } else {
-        // set player lives
-        const newPlayerHealth = playerHealthTracker;
-        newPlayerHealth.pop();
-        setPlayerHealthTracker(newPlayerHealth);
-        if (playerHealthTracker.length == 0) {
-          setGameOver(true);
-          setPlayerLost(true);
-        }
+      // Checking rounding error for geometry questiosn involving PI
+      if (category == "geometry" && gauntletQuestion.solution.length > 5) {
+        const solution =
+          Math.round(
+            Number(gauntletQuestion.solution.replace(/\s+/g, "")) * 100
+          ) / 100;
 
-        // Reset bools
-        setQuestionComplete(true);
-        setQuestionCorrect(false);
+        console.log(solution);
+        console.log(Number(userAnswer));
+
+        if (
+          Number(userAnswer) >= solution + 0.2 ||
+          Number(userAnswer) <= solution + 0.2
+        ) {
+          if (bossHealth - 10 == 0) {
+            playVictorySound();
+            setGameOver(true);
+            setDefeatedMonster(true);
+            updateSouls();
+          } else {
+            setBossHealth(bossHealth - 10);
+            setQuestionComplete(true);
+            setQuestionCorrect(true);
+          }
+        } else {
+          // set player lives
+          const newPlayerHealth = playerHealthTracker;
+          newPlayerHealth.pop();
+          setPlayerHealthTracker(newPlayerHealth);
+          if (playerHealthTracker.length == 0) {
+            setGameOver(true);
+            setPlayerLost(true);
+            playDeathSound();
+          }
+
+          // Reset bools
+          setQuestionComplete(true);
+          setQuestionCorrect(false);
+        }
+      }
+      // Checking everything else
+      else {
+        if (
+          userAnswer.replace(/\s+/g, "") ==
+          gauntletQuestion.solution.replace(/\s+/g, "")
+        ) {
+          if (bossHealth - 10 == 0) {
+            console.log("play sound");
+            playVictorySound();
+            setGameOver(true);
+            setDefeatedMonster(true);
+            updateSouls();
+          } else {
+            setBossHealth(bossHealth - 10);
+            setQuestionComplete(true);
+            setQuestionCorrect(true);
+          }
+        } else {
+          // set player lives
+          const newPlayerHealth = playerHealthTracker;
+          newPlayerHealth.pop();
+          setPlayerHealthTracker(newPlayerHealth);
+          if (playerHealthTracker.length == 0) {
+            setGameOver(true);
+            setPlayerLost(true);
+            playSound();
+          }
+
+          // Reset bools
+          setQuestionComplete(true);
+          setQuestionCorrect(false);
+        }
       }
     }
   };
 
   const checkAnswerKeyDown = () => {
     if (event.key == "Enter") {
+      //   console.log(userAnswer.replace(/\s+/g, ""));
+      //   console.log(gauntletQuestion.solution.replace(/\s+/g, ""));
       if (userAnswer.trim() == "") {
         alert("Answer box is empty!");
       } else {
-        if (
-          userAnswer.trim() == gauntletQuestion.solution.trim() &&
-          questionComplete == false
-        ) {
-          setBossHealth(bossHealth - 10);
-          questionComplete(true);
-          setUserAnswer("");
-        } else {
-          nextQuestion();
+        // Checking rounding error for geometry questiosn involving PI
+        if (category == "geometry" && gauntletQuestion.solution.length > 5) {
+          const solution =
+            Math.round(
+              Number(gauntletQuestion.solution.replace(/\s+/g, "")) * 100
+            ) / 100;
+
+          console.log(solution);
+          console.log(Number(userAnswer));
+
+          if (
+            Number(userAnswer) >= solution + 0.2 ||
+            Number(userAnswer) <= solution + 0.2
+          ) {
+            if (bossHealth - 10 == 0) {
+              playVictorySound();
+              setGameOver(true);
+              setDefeatedMonster(true);
+              updateSouls();
+            } else {
+              setBossHealth(bossHealth - 10);
+              setQuestionComplete(true);
+              setQuestionCorrect(true);
+            }
+          } else {
+            // set player lives
+            const newPlayerHealth = playerHealthTracker;
+            newPlayerHealth.pop();
+            setPlayerHealthTracker(newPlayerHealth);
+            if (playerHealthTracker.length == 0) {
+              setGameOver(true);
+              setPlayerLost(true);
+              playDeathSound();
+            }
+
+            // Reset bools
+            setQuestionComplete(true);
+            setQuestionCorrect(false);
+          }
+        }
+        // Checking everything else
+        else {
+          if (
+            userAnswer.replace(/\s+/g, "") ==
+            gauntletQuestion.solution.replace(/\s+/g, "")
+          ) {
+            if (bossHealth - 10 == 0) {
+              playVictorySound();
+              setGameOver(true);
+              setDefeatedMonster(true);
+              updateSouls();
+            } else {
+              setBossHealth(bossHealth - 10);
+              setQuestionComplete(true);
+              setQuestionCorrect(true);
+            }
+          } else {
+            // set player lives
+            const newPlayerHealth = playerHealthTracker;
+            newPlayerHealth.pop();
+            setPlayerHealthTracker(newPlayerHealth);
+            if (playerHealthTracker.length == 0) {
+              setGameOver(true);
+              setPlayerLost(true);
+              playSound();
+            }
+
+            // Reset bools
+            setQuestionComplete(true);
+            setQuestionCorrect(false);
+          }
         }
       }
     }
@@ -112,6 +233,35 @@ const Gauntlet = ({ setUserUpdate }) => {
     setQuip(newQuip);
   };
 
+  const updateSouls = async () => {
+    // fetching current souls
+    let { data: Users, error } = await supabase
+      .from("Users")
+      .select("*")
+      .eq("userID", localStorage.getItem("userID"));
+
+    var newSouls = Number(Users[0].souls);
+
+    // Adding correct soul amount
+    if (category == "basic_math") {
+      newSouls = newSouls + 20;
+    } else if (category == "algebra") {
+      newSouls = newSouls + 30;
+    } else if (category == "geometry") {
+      newSouls = newSouls + 40;
+    }
+
+    // console.log(newSouls);
+
+    const { data, error: soulsError } = await supabase
+      .from("Users")
+      .update({ souls: newSouls.toString() })
+      .eq("userID", localStorage.getItem("userID"))
+      .select();
+
+    setUserUpdate(newSouls);
+  };
+
   useEffect(() => {
     fetchGauntletQuestion();
 
@@ -121,6 +271,9 @@ const Gauntlet = ({ setUserUpdate }) => {
       hearts.push(i);
     }
     setPlayerHealthTracker(hearts);
+
+    // setting a quip
+    updateQuip();
   }, []);
 
   return (
@@ -128,17 +281,69 @@ const Gauntlet = ({ setUserUpdate }) => {
       {!gameOver ? (
         <>
           <div className="gaunlet-question-container">
-            <div className="gauntlet-question">{gauntletQuestion.problem}</div>
+            {category == "basic_math" && (
+              <div className="gauntlet-question">
+                {gauntletQuestion.problem} =
+              </div>
+            )}
+            {category == "algebra" && (
+              <div
+                className="gauntlet-question"
+                style={{
+                  fontSize: "40px",
+                }}
+              >
+                {gauntletQuestion.problem}
+              </div>
+            )}
+            {category == "geometry" && (
+              <div
+                className="gauntlet-question"
+                style={{
+                  fontSize: "40px",
+                }}
+              >
+                {gauntletQuestion.problem}
+              </div>
+            )}
             <div className="gauntlet-question-type">
               {gauntletQuestion.type}
             </div>
-            <input
-              type="text"
-              className="gauntlet-question-input"
-              value={userAnswer}
-              onChange={(event) => setUserAnswer(event.target.value)}
-              onKeyDown={checkAnswerKeyDown}
-            />
+            <div
+              className="gauntlet-question-type"
+              style={{
+                fontSize: "16px",
+                color: "gray",
+              }}
+            >
+              {gauntletQuestion.type == "Combining Like Terms" &&
+                "Example answer: 10x+16x^2 (lower order terms first!)"}
+              {gauntletQuestion.type == "Expanding Polynomials" &&
+                "Example answer: 36x^2-12x (higher order terms first!"}
+              {gauntletQuestion.type == "Linear Equations" &&
+                "Example answer: x,y or -5,-1"}
+              {gauntletQuestion.type == "System of Equations" &&
+                "Example answer: x,y or -5,-1"}
+              {category == "geometry" &&
+                "Round TWO decimal places. Example: 12.43"}
+            </div>
+            {!questionComplete ? (
+              <input
+                type="text"
+                className="gauntlet-question-input"
+                value={userAnswer}
+                onChange={(event) => setUserAnswer(event.target.value)}
+                onKeyDown={checkAnswerKeyDown}
+              />
+            ) : (
+              <input
+                type="text"
+                className="gauntlet-question-input"
+                value={userAnswer}
+                onChange={(event) => setUserAnswer(event.target.value)}
+                onKeyDown={nextQuestion}
+              />
+            )}
             <div className="gauntlet-solution">
               {questionComplete && questionCorrect && "Correct!"}
               {questionComplete &&
@@ -159,6 +364,9 @@ const Gauntlet = ({ setUserUpdate }) => {
           <div className="gauntlet-boss-container">
             <div className="gauntlet-boss-name">
               {category == "basic_math" && "The Fundamental Fury"}
+              {category == "algebra" && "Lord Algebra, The Unknown"}
+              {category == "geometry" &&
+                "Lucious Cube, The Man With the Six Faces"}
             </div>
             <Box sx={{ width: "100%" }}>
               <LinearProgress
@@ -187,13 +395,23 @@ const Gauntlet = ({ setUserUpdate }) => {
           </div>
         </>
       ) : (
-        <div
-          style={{
-            color: "white",
-          }}
-        >
-          {playerLost && <div>You have fallen.</div>}
-          {defeatedMonster && <div>Monster felled.</div>}
+        <div className="gauntlet-end">
+          {playerLost && (
+            <div className="gauntlet-fallen">YOU HAVE FALLEN.</div>
+          )}
+          {defeatedMonster && (
+            <div
+              className="gauntlet-fallen"
+              style={{
+                color: "gold",
+              }}
+            >
+              MONSTER FELLED.
+            </div>
+          )}
+          <button className="gauntlet-return" onClick={() => navigate("/")}>
+            Return Home
+          </button>
         </div>
       )}
     </div>
