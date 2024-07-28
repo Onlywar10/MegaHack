8d3c3426a05b159853d714482a365905ae0826fe
hubbbbbbbbb <hubyu9234@gmail.com>
2024-07-28 02:32:49 -0700

    quick formatting

--- math-souls/src/pages/Home.jsx
+++ math-souls/src/pages/Home.jsx
@@ -1,30 +1,28 @@
 import React from "react";
-import './Home.css';
-import { useNavigate } from 'react-router-dom';
-import devil from '../assets/Devil_Fruits_10.png';
+import "./Home.css";
+import { useNavigate } from "react-router-dom";
+import devil from "../assets/Devil_Fruits_10.png";
 
 const Home = () => {
   const navigate = useNavigate();
-  
+
   function handleClick() {
-    navigate('/question');
+    navigate("/question");
   }
-  function toList(){
-    navigate('/list');
+  function toList() {
+    navigate("/list");
   }
 
   return (
     <body>
-      <p className="titl">
-        Welcome player! What would you like to do?
-      </p>
+      <p className="titl">Welcome player! What would you like to do?</p>
       <div className="buttons">
         <a onClick={toList} class="image-container">
-          <img src={devil} alt="Local Image" style={{border: 'none'}}/>
+          <img src={devil} alt="Local Image" style={{ border: "none" }} />
           <div class="overlay-text">Tackle an enemy</div>
         </a>
         <a onClick={handleClick} class="image-container">
-          <img src={devil} alt="Local Image" style={{border: 'none'}}/>
+          <img src={devil} alt="Local Image" style={{ border: "none" }} />
           <div class="overlay-text">Tackle the boss</div>
         </a>
       </div>
@@ -33,11 +31,11 @@ const Home = () => {
 };
 
 const button2 = () => {
-  return(
+  return (
     <div>
       <button>button2</button>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
